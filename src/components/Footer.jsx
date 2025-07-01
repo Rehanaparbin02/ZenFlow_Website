d9418de78fbaf260e8d61d0801e161422b1652e6
Rehana Parbin <rehanaparbin0210@gmail.com>
2025-07-01 11:11:10 +0530

    react router omitted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import './css/footer.css';
 
 const Footer = () => {
@@ -13,10 +11,10 @@ const Footer = () => {
 
         <div className="footer-links">
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/features">Features</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li>Home</li>
+            <li>Features</li>
+            <li>About</li>
+            <li>Contact</li>
           </ul>
         </div>
 
